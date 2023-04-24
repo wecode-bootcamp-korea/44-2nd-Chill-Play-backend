69ce2f85b5e16f25bc6d8c358dc70812fb428053
naturerest <asdqwe747@gmail.com>
2023-04-24 20:06:03 +0900

    musicals,theaters 테이블 칼럼 추가

--- db/migrations/20230424062524_create_chill_play_theaters_table.sql
+++ db/migrations/20230424062524_create_chill_play_theaters_table.sql
@@ -5,6 +5,7 @@ CREATE TABLE theaters
     name VARCHAR(100) NOT NULL,
     total_seats INT NOT NULL,
     city_id INT NOT NULL,
+    thumbnail_image_url VARCHAR(2000) NOT NULL,
     CONSTRAINT FK_theaters_city_id  FOREIGN KEY(city_id) REFERENCES cities(id)
 );
 
--- db/migrations/20230424062810_create_chill_play_musicals_table.sql
+++ db/migrations/20230424062810_create_chill_play_musicals_table.sql
@@ -3,11 +3,12 @@ CREATE TABLE musicals
 (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    descriptions VARCHAR(500) NOT NULL,
+    descriptions VARCHAR(300) NOT NULL,
     post_image_url VARCHAR(2000) NOT NULL,
     released_date DATE NOT NULL,
     end_date DATE NOT NULL,
     running_time INT NOT NULL,
+    synopsis VARCHAR(1500) NOT NULL,
     age_rated_id INT NOT NULL,
     boarding_status_id INT NOT NULL,
     theater_id INT NOT NULL,

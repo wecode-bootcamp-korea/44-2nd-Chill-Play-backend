6556abaa58cca853c865d38e097c5df87aa96867
Lim seong-geun <123074891+lsg622@users.noreply.github.com>
2023-04-28 21:43:36 +0900

    wip

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-<<<<<<< HEAD
-const router = require('./routes');
-=======
 const router = require('./routes')
->>>>>>> bd6813a ([ADD]: feature/search)
 
 const { globalErrorHandler } = require('./utils/error');
 
@@ -17,12 +13,8 @@ const createApp = () => {
   app.use(cors());
   app.use(express.json());
   app.use(morgan('combined'));
-<<<<<<< HEAD
   app.use(router);
   app.use(globalErrorHandler);
-=======
-  app.use(router)
->>>>>>> bd6813a ([ADD]: feature/search)
 
   app.get('/ping', function (req, res, next) {
     res.status(200).json({ message: 'pong' });
